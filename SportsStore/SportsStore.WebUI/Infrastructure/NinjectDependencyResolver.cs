1886afa0eed7ef17f6f86b3692697ae45f70813f
Piotr Kaźmierczak <pkazmierczak13@gmail.com>
2017-01-26 21:09:13 +0100

    Testowe repozytorium danych, prosty widok wyświetlające je

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //umieścić dodatkowe powiązania
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+            new Product { Name = "Piłka nożna", Price = 25 },
+            new Product { Name = "Deska surfingowa", Price = 179 },
+            new Product { Name = "Buty do biegania", Price = 95 }
+            });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
