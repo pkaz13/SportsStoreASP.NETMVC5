7445cde61306d2abae34d7a81aa8d74b319b05f0
Piotr Kaźmierczak <pkazmierczak13@gmail.com>
2017-01-28 13:28:05 +0100

    Dokończenie implementacji koszyka

--- SportsStore/SportsStore.UnitTests/CartTests.cs
+++ SportsStore/SportsStore.UnitTests/CartTests.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace SportsStore.UnitTests
 {
@@ -120,6 +125,71 @@ namespace SportsStore.UnitTests
 
             // asercje
             Assert.AreEqual(target.Lines.Count(), 0);
+        }
+
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+
+            // przygotowanie - tworzenie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Jabłka"},
+            }.AsQueryable());
+
+            // przygotowanie - tworzenie koszyka
+            Cart cart = new Cart();
+
+            // przygotowanie - utworzenie kontrolera
+            CartController target = new CartController(mock.Object);
+
+            // działanie - dodanie produktu do koszyka
+            target.AddToCart(cart, 1, null);
+
+            // asercje
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
+        }
+
+        [TestMethod]
+        public void Adding_Product_To_Cart_Goes_To_Cart_Screen()
+        {
+            // przygotowanie - tworzenie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Jabłka"},
+            }.AsQueryable());
+
+            // przygotowanie - tworzenie koszyka
+            Cart cart = new Cart();
+
+            // przygotowanie - utworzenie kontrolera
+            CartController target = new CartController(mock.Object);
+
+            // działanie - dodanie produktu do koszyka
+            RedirectToRouteResult result = target.AddToCart(cart, 2, "myUrl");
+
+            // asercje
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+
+        [TestMethod]
+        public void Can_View_Cart_Contents()
+        {
+            // przygotowanie - tworzenie koszyka
+            Cart cart = new Cart();
+
+            // przygotowanie - utworzenie kontrolera
+            CartController target = new CartController(null);
+
+            // działanie - wywołanie metody akcji Index
+            CartIndexViewModel result
+                = (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+
+            // asercje
+            Assert.AreSame(result.Cart, cart);
+            Assert.AreEqual(result.ReturnUrl, "myUrl");
         }
     }
 }
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,36 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            //pobranie obiektu Cart z sesji
+            Cart cart = null;
+            if(controllerContext.HttpContext.Session != null)
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            }
+
+            //utworzenie obiektu Cart, jeśli nie został znaleziony w danych sesji
+            if(cart == null)
+            {
+                cart = new Cart();
+                if(controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+            //zwróć koszyk
+            return cart;
+        }
+    }
+}
